8a7144e505bce7142fea28c65c806be2d452a2c7
Facundo Scoccia <facundo.scoccia@globant.com>
2020-09-19 11:13:00 -0400

    add add simple gui

---
+++ app/src/main/java/io/github/fanky10/recipies/MainActivity.kt
@@ -0,0 +1,30 @@
+package io.github.fanky10.recipies
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import io.github.fanky10.recipies.domain.Recipie
+
+class MainActivity : AppCompatActivity() {
+
+    private val recipie: Recipie = RecipiesRepository.get()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.txtTitle).text = recipie.name
+        findViewById<TextView>(R.id.txtIngredientsList).text = getIngredientsContent().toString()
+        findViewById<TextView>(R.id.txtStepsList).text = getStepsContent()
+    }
+
+    private fun getIngredientsContent() = recipie.ingredients.map {
+        "${it.quantity} ${it.name}\n"
+    }
+
+    private fun getStepsContent() = recipie.steps
+        .sortedBy { it.order }
+        .map {
+            "(${it.order}) - ${it.description}\n"
+        }.toString()
+}
