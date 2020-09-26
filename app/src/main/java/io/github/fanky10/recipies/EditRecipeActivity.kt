45f5a33cbcc2e03c74b6b67df958dacd3c6533cc
2Davit <71561686+2Davit@users.noreply.github.com>
2020-09-26 16:17:41 -0300

    added new labels on edit form for ingredients, quantity and steps, and make it editable
    changed Recipe class ingredients an steps lists to MutableLists.

--- app/src/main/java/io/github/fanky10/recipies/EditRecipeActivity.kt
+++ app/src/main/java/io/github/fanky10/recipies/EditRecipeActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import io.github.fanky10.recipies.domain.Ingredient
 import io.github.fanky10.recipies.domain.Recipe
+import io.github.fanky10.recipies.domain.Step
 
 class EditRecipeActivity : AppCompatActivity() {
 
@@ -14,14 +16,26 @@ class EditRecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_recipe)
+
         findViewById<EditText>(R.id.etName).setText(recipe.name)
+        findViewById<EditText>(R.id.etIng1).setText(recipe.ingredients[0].name)
+        findViewById<EditText>(R.id.etIng1Quantity).setText(recipe.ingredients[0].quantity)
+        findViewById<EditText>(R.id.etStep1).setText(recipe.steps[0].description)
+
         findViewById< Button>(R.id.btnSave).setOnClickListener {
             save()
         }
     }
 
     private fun save() {
-        RecipiesRepository.saveName(findViewById<EditText>(R.id.etName).text.toString())
+        val ing1: String = findViewById<EditText>(R.id.etIng1).text.toString()
+        val ingQuantity: String = findViewById<EditText>(R.id.etIng1Quantity).text.toString()
+        val step1: String = findViewById<EditText>(R.id.etStep1).text.toString()
+
+        val ingredient1 = Ingredient(ing1, ingQuantity)
+        val stepNumber1 = Step(1, step1)
+
+        RecipiesRepository.saveAll(findViewById<EditText>(R.id.etName).text.toString(), ingredient1, stepNumber1)
         finish()
     }
 }
--- app/src/main/java/io/github/fanky10/recipies/RecipiesRepository.kt
+++ app/src/main/java/io/github/fanky10/recipies/RecipiesRepository.kt
@@ -7,36 +7,37 @@ import io.github.fanky10.recipies.domain.Step
 object RecipiesRepository {
     // from: https://cookpad.com/eeuu/recetas/5999222-chuletas-de-cerdo-con-papas-y-romero-al-horno
     private var recipe: Recipe = Recipe(
-        "Chuletas de cerdo con papas y romero al horno",
-        listOf(
-            Ingredient("chuletas de cerdo", "4"),
-            Ingredient("papas", "2"),
-            Ingredient("cebolla", "1"),
-            Ingredient("sal y pimienta", "a gusto"),
-            Ingredient("romero", "a gusto"),
-            Ingredient("salsa de soja", "1 chorrito"),
-            Ingredient("aceite de oliva", "1 chorrito")
-        ),
-        listOf(
-            Step(
-                1, "Precalentar el horno. En un bol marinar las chuletas con la sal, pimienta, ajo en polvo, la mitad del romero, aceite de oliva y la salsa de soja"
+            "Chuletas de cerdo con papas y romero al horno",
+            mutableListOf(
+                    Ingredient("chuletas de cerdo", "4"),
+                    Ingredient("papas", "2"),
+                    Ingredient("cebolla", "1"),
+                    Ingredient("sal y pimienta", "a gusto"),
+                    Ingredient("romero", "a gusto"),
+                    Ingredient("salsa de soja", "1 chorrito"),
+                    Ingredient("aceite de oliva", "1 chorrito")
             ),
-            Step(
-                2, "En una fuente colocamos la papa cortada en rodajas y la cebolla cortada en julianas. Todo condimentado con sal, pimienta, la mitad del romero y aceite de oliva. Mezclar bien"
-            ),
-            Step(
-                3, "Añadir las chuletas encima de las papas y llevar al horno a fuego medio. A media cocción (aprox unos 15 min) sacar y dar vuelta a las chuletas y dejar 15 min más"
+            mutableListOf(
+                    Step(
+                            1, "Precalentar el horno. En un bol marinar las chuletas con la sal, pimienta, ajo en polvo, la mitad del romero, aceite de oliva y la salsa de soja"
+                    ),
+                    Step(
+                            2, "En una fuente colocamos la papa cortada en rodajas y la cebolla cortada en julianas. Todo condimentado con sal, pimienta, la mitad del romero y aceite de oliva. Mezclar bien"
+                    ),
+                    Step(
+                            3, "Añadir las chuletas encima de las papas y llevar al horno a fuego medio. A media cocción (aprox unos 15 min) sacar y dar vuelta a las chuletas y dejar 15 min más"
+                    )
             )
-        )
     )
 
     fun get() = recipe
 
-    fun saveName(name: String) {
-        recipe = Recipe(
-            name,
-            recipe.ingredients,
-            recipe.steps
+    fun saveAll(name: String, ing: Ingredient, step: Step) {
+        recipe = Recipe(name,
+                recipe.ingredients,
+                recipe.steps
         )
+        recipe.ingredients[0] = ing
+        recipe.steps[0] = step
     }
 }
